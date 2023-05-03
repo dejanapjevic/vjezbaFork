61a29cd2ac3570d2c28d05518cfae3234f1516d5
Dejana <dejanapjevic@hotmail.com>
2023-05-03 18:54:49 +0200

    java inicijalni projekat
    dodata funkcija ispis

---
+++ dejanaProjekat/src/dejanaProjekat/MainClass.java
@@ -0,0 +1,14 @@
+package dejanaProjekat;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Auto auto1 = new Auto("Golf", 1234);
+		Auto auto2 = new Auto("Passat", 2084);
+		System.out.println(auto1.getBrzina());
+		System.out.println(auto2.ispis());
+
+	}
+
+}
